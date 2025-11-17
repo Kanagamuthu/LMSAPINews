80cf8d7fee81ee95ea49be3890a766872af27f8c
dhamu <User@UKRECCAROLIN>
2025-11-17 13:53:09 +0530

    validation

--- LmsAPI/obj/Debug/net8.0/LMSAPI.AssemblyInfo.cs
+++ LmsAPI/obj/Debug/net8.0/LMSAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24727c3ee07972de1383b27137274c878abf9312")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
